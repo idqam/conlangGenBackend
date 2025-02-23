48f59c396d5bfb4ddcebcd371bdcf68612297f8b
idqam <owenjave5@gmail.com>
2025-02-23 18:04:29 -0500

    allow origins

--- app/main.py
+++ app/main.py
@@ -12,7 +12,8 @@ app.include_router(api_router, prefix="/api/v1")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins (for local dev)
+    allow_origins=["https://conlang-gen-front-fw57pjdni-owen-villareals-projects.vercel.app",
+        "https://conlang.lat", "https://www.conlang.lat"],  # Allow all origins (for local dev)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
